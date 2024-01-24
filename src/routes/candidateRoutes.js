ecebb65c7b3d33db6678043130635f6dfd815c22
JeisonNovoa <jeisonnovoa7@gmail.com>
2024-01-24 17:22:46 -0500

    enable CORS for all routes

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const multer = require("multer");
+const cors = require("cors");
 const upload = multer({ storage: multer.memoryStorage() });
 const { processCandidates } = require("../controllers/candidateController");
 
 const router = express.Router();
+router.use(cors());
 
 router.post("/analyze", upload.array("resumes", 3), processCandidates);
 
